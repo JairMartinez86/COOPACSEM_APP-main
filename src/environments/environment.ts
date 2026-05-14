6a2b4ce078a0831ebb156f15b70dc34ac6ba0ca6
JairMartinez86 <jmartinezg86@gmail.com>
2026-05-13 23:30:12 -0600

    mejoras

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
-export const environment = {
+/*export const environment = {
 
   //apiUrl: 'http://localhost:5251/api', // o tu dominio
   //apiUrl: 'https://gh6wc6dg-5251.use.devtunnels.ms/api'
   production: false,
   apiUrl: '/api'
 };
+*/
 
-
-/*export const environment = {
+export const environment = {
   production: true,
   apiUrl: 'https://coopacsem-api-main.onrender.com/api'
-};*/
+};
